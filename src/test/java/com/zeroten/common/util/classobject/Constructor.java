ebd8bb3f193987f95edfcb70d5474c7884c3e358
qianjl-star <1648182671@qq.com>
2019-12-18 18:04:07 +0800

    构造器的使用

---
+++ src/test/java/com/zeroten/common/util/classobject/Constructor.java
@@ -0,0 +1,41 @@
+package com.zeroten.common.util.classobject;
+
+public class Constructor {
+    public static int count = 0;
+    public static int index = 1;
+    // 构造器、构造方法
+    public Constructor() {
+        System.out.println("无参构造");
+    }
+    public Constructor(int count) {
+        this();
+        System.out.println("有参构造 1");
+        System.out.println("    传入的 count：" + count);
+        System.out.println("    类状态/属性 count：" + this.count);
+        this.count = count;
+    }
+    public Constructor(int count, int index) {
+        this(count);
+        this.index = index;
+        System.out.println("有参构造 2");
+        System.out.println("    嵌套 this 调用");
+    }
+
+    // 方法，可以与类名相同
+    public void Constructor() {
+    }
+
+    public static void main(String[] args) {
+        // 构造方法不能直接引用，在创建类的时候使用
+        Constructor con1 = new Constructor();
+        System.out.println();
+
+        Constructor con2 = new Constructor(11);
+        System.out.println("    this 赋值域变量后类状态/属性 count：" + Constructor.count);
+        System.out.println();
+
+        Constructor con3 = new Constructor(33, 34);
+        System.out.println("    count :" + Constructor.count);
+        System.out.println("    index :" + Constructor.index);
+    }
+}
