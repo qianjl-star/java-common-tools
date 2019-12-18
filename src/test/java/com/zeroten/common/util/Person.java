cabc193af5e35c0e597aa69e45f6d375b5cf60ff
qianjl-star <1648182671@qq.com>
2019-12-18 16:39:25 +0800

    类和对象的定义

---
+++ src/test/java/com/zeroten/common/util/Person.java
@@ -0,0 +1,8 @@
+package com.zeroten.common.util;
+
+public class Person {
+    public String person = "属性";
+    public void person(){
+        String person = "动作";
+    }
+}
---
+++ src/test/java/com/zeroten/common/util/PersonTest.java
@@ -0,0 +1,7 @@
+package com.zeroten.common.util;
+
+public class PersonTest {
+    Person p1 = new Person();
+    Person p2 = new Person();
+    Person p3 = new Person();
+}
